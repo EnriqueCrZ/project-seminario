2bc441c3e07decb14521cfd06e07915e54aad81c
Jgarcim31 <jose123.garciamadrid@gmail.com>
2021-10-31 16:57:58 -0600

    se agrego vistas vacias en carpeta mantenimientos

--- resources/views/mantenimiento/estatusmantenimiento.blade.php
+++ resources/views/mantenimiento/estatusmantenimiento.blade.php
@@ -1,9 +1,23 @@
 @extends('layouts.admin')
 
 @section('main-content')
-    <h2>Estatus de Mantenimiento</h2>
+    <h2>ESTADO DE MANTENIMIENTO</h2>
 
-
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="mantenimiento.blade.php">MANTENIMIENTO</a>
+        </li>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="estatusmantenimiento.blade.php.blade.php">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="proximosmantenimiento.blade.php">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="insumosrespuestos.blade.php">Link</a>
+        </li>
+    </ul>
 
     <button type="button" class="btn btn-primary">Crear Orden de Trabajo</button>
 
