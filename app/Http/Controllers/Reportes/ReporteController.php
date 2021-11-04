10063ae5c5ae4925bb106ee6bbb32e7ec5b65203
EnriqueCrZ <enriquecrz21@gmail.com>
2021-11-04 17:25:39 -0600

    Preubas de pdf

--- app/Http/Controllers/Reportes/ReporteController.php
+++ app/Http/Controllers/Reportes/ReporteController.php
@@ -6,8 +6,10 @@ use App\Exports\ProviderExport;
 use App\Exports\UsuarioExport;
 use App\Exports\VehicleExport;
 use App\Http\Controllers\Controller;
+use App\Http\Models\Provider;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Maatwebsite\Excel\Facades\Excel;
 
 class ReporteController extends Controller
@@ -40,7 +42,7 @@ class ReporteController extends Controller
         if($request->exportType == self::$EXCEL)
             return self::selectCollectionExcelExport($request->collection,$hoy);
         else
-            return $request;
+            return self::selectCollectionPDFExport($request->collection,$hoy);
     }
 
     private static function selectCollectionExcelExport($collection,$hoy){
@@ -54,6 +56,18 @@ class ReporteController extends Controller
             return 0;
     }
 
+    private static function selectCollectionPDFExport($collection,$hoy){
+        if($collection == self::$VEHICLES)
+            return 0;
+        elseif($collection == self::$USERS)
+            return 0;
+        elseif($collection == self::$PROVIDERS)
+            return self::exportPDFProviders($hoy);
+        else
+            return 0;
+
+    }
+
     private static function exportExcelVehicles($hoy){
         return Excel::download(new VehicleExport(),'Vehiculos-'.$hoy.'.xlsx');
     }
@@ -64,5 +78,20 @@ class ReporteController extends Controller
 
     private static function exportExcelProviders($hoy){
         return Excel::download(new ProviderExport(),'Proveedores-'.$hoy.'.xlsx');
+    }
+
+    /*
+     * Funciones para la generacion de PDF
+     */
+
+    private static function exportPDFProviders($hoy){
+        $providers = Provider::all();
+        $pdf = App::make('dompdf.wrapper');
+        $view = view('reportes.pdf.provider',compact('providers','hoy'));
+
+        $pdf->loadHTML($view);
+
+        $pdf->setOptions(['isPhpEnabled'=>true,'isRemoteEnabled'=>true]);
+        return $pdf->stream('Proveedores'.$hoy.'.pdf');
     }
 }
